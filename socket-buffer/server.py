cdaf62ccea328fc5383fc35f5a5b884a62cf3997
Shubhankar Deskpande <shubhankardeshpande.14@gmail.com>
2023-03-10 22:02:52 -0500

    Commiting buffer and header size of socket programming

--- socket-buffer/server.py
+++ socket-buffer/server.py
@@ -1,4 +1,6 @@
-import socket
+import socket 
+import time
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
@@ -11,7 +13,11 @@ s.listen(5)
 while True:
     clientsocket , adress = s.accept()
     print(f"connection from {adress} has been established")
-    clientsocket.send(bytes("Welcome to server","utf-8"))
-    clientsocket.close()
+    while True:
+        time.sleep(3)
+        msg = f"time is{time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}"+msg
+        clientsocket.send(bytes(msg,"utf-8"))
+    
      
 
